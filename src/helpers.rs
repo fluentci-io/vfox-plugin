28f403d74c1cba37b063daba91fa834a9d0eb405
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-07-10 09:36:19 +0000

    set GITHUB_ACCESS_TOKEN

--- src/helpers.rs
+++ src/helpers.rs
@@ -7,9 +7,18 @@ pub fn setup() -> Result<String, Error> {
     let shims = format!("{}/.version-fox/shims", home);
     dag().set_envs(vec![("PATH".into(), format!("{}:{}", shims, path))])?;
 
+    let gh_token = dag().get_env("GITHUB_ACCESS_TOKEN")?;
+
+    if !gh_token.is_empty() && !gh_token.starts_with("token ") {
+        dag().set_envs(vec![(
+            "GITHUB_ACCESS_TOKEN".into(),
+            format!("token {}", gh_token),
+        )])?;
+    }
+
     let stdout = dag()
         .pkgx()?
-        .with_exec(vec!["type vfox > /dev/null || pkgx curl -sSL https://raw.githubusercontent.com/version-fox/vfox/main/install.sh | bash"])?
+        .with_exec(vec!["type vfox > /dev/null || pkgx curl -sSL https://raw.githubusercontent.com/fluentci-io/vfox-installer/main/install.sh | bash"])?
         .stdout()?;
     Ok(stdout)
 }
